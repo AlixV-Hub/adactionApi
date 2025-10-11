0978a62a77761151b2751df3663e6bea356ee772
Alix VEYRAT <alixveyrat@gmail.com>
2025-10-11 17:33:28 +0200

    fix : volonteer add forms in volonteerController with CrossOrigin

--- src/main/java/Al/Adaction/demo/controller/volonteer/VolonteerController.java
+++ src/main/java/Al/Adaction/demo/controller/volonteer/VolonteerController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/volonteers")
+@CrossOrigin(origins = "http://localhost:4200")
 public class VolonteerController {
 
     private final IVolonteerService volonteerService;
